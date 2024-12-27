318c4aaafac0f37cebf1722b2eaf5f4cc93cae37
ComTech <shumailagulfam3@gmail.com>
2024-12-27 23:12:12 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import BookCards from "@/components/bookCards";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-center text-4xl font-bold mb-8 text-white">Welcome to Book Store</h1>
+
+      <BookCards href={"/register"} func={()=>{}}/>
+    </>
+  );
+}
